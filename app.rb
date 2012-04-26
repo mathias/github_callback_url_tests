b4b8c9aede249ecb46102a2437142d56bef8b4cf
Matt Gauger <matt.gauger@gmail.com>
2012-04-26 02:37:51 -0500

    YAY

---
+++ app.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'sinatra'
+
+class DumbererApp < Sinatra::Base
+  # use Rack::CommonLogger for request logging
+
+  # the main endpoint for scm services
+  post '/' do
+    # info "Handling ping for #{credentials.inspect}"
+    puts data
+    puts "Request created : #{payload.inspect}"
+    204
+  end
+
+  protected
+
+  def data
+    {
+      :type => event_type,
+      #:credentials => credentials,
+      :request => payload
+    }
+  end
+
+  def event_type
+    env['HTTP_X_GITHUB_EVENT'] || 'UNKNOWN'
+  end
+
+  def credentials
+    login, token = Rack::Auth::Basic::Request.new(env).credentials
+    { :login => login, :token => token }
+  end
+
+  def payload
+    params[:payload]
+  end
+end
